ec5bef4d154dcc64413b8d21648ad422e6b79fd9
camille-haxa <camille.haxa@proton.me>
2026-04-20 15:26:39 +0200

    feat: extraction url de la page et de l'image

--- p2.py
+++ p2.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+#récupérer infos pour 1 produit
 url = "https://books.toscrape.com/catalogue/shakespeares-sonnets_989/"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
@@ -10,12 +11,15 @@ title = doc.title.string
 print("title:", title)
 
 #extraction url product page
+page_url = url
+print("url:", page_url)
 
 #extraction upc
 upc = doc.find("td")
 print("upc:", upc.text)
 
 #price including tax
+
 
 #price excluding tax
 
@@ -25,6 +29,14 @@ print("upc:", upc.text)
 product_description_div = doc.find(id="product_description")
 product_description = product_description_div.find_next_sibling("p")
 print("description produit:", product_description.text)
+
+#category
+
+#review rating
+
+#image url
+image = doc.img['src']
+print("image url:", image)
 
 # voir le code parsé par beautiful soup
 #print(doc)
