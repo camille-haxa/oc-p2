04351ece02dd97a0f8df502e5f57e841c3ef00ae
camille-haxa <camille.haxa@proton.me>
2026-04-27 09:46:26 +0200

    feat: extraction prix, category et amelioration code pour upc et titre

--- p2.py
+++ p2.py
@@ -7,7 +7,7 @@ page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
 #extraction du titre de la page
-title = doc.title.string
+title = doc.h1.string
 print("title:", title)
 
 #extraction url product page
@@ -15,17 +15,20 @@ page_url = url
 print("url:", page_url)
 
 #extraction upc
-upc = doc.find("td")
-print("upc:", upc.text)
+upc = doc.find(text="UPC")
+print("upc:", upc.next.text)
 
 #price including tax
-
+price_including_tax = doc.find(text="Price (incl. tax)")
+print("prix TTC:", price_including_tax.next.text)
 
 #price excluding tax
+price_excluding_tax = doc.find(text="Price (excl. tax)")
+print("prix HT:", price_excluding_tax.next.text)
 
 #number avalaible
-availability = doc.find("tr:nth-child(5)")
-print("number available:", availability)
+#availability = doc.find(text="Availability")
+#print("number available:", availability.next.text)
 
 #extraction product description
 product_description_div = doc.find(id="product_description")
@@ -33,12 +36,12 @@ product_description = product_description_div.find_next_sibling("p")
 print("description produit:", product_description.text)
 
 #category
-category_list = doc.select("ul > li:nth-of-type(3)", limit=1)
-print("category:", category_list)
+category_list = doc.find(class_="breadcrumb").find_all('li')[2]
+print("category:", category_list.text.strip())
 
 #review rating
-rating = doc.find("p", class_="star-rating Four")
-print("review rating:", rating)
+#rating = doc.find("p", class_="star-rating Four")
+#print("review rating:", rating)
 
 #image url
 image = doc.img['src']
