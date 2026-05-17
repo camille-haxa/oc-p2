f0e9edd8747790b5e50399028064c1972917b08c
camille-haxa <camille.haxa@proton.me>
2026-05-17 16:05:13 +0200

    feat: extrait titre livre, créer fonction pour recuperer données de chaque livre sur son url

--- phase2.py
+++ phase2.py
@@ -15,13 +15,30 @@ all_books = doc.find_all('article', class_='product_pod')
 #print("all books:", all_books)
 #TODO:extraire toutes les pages de la catégorie
 
-#faire une boucle pour récupèrer les urls de chaque livre
+#definir une fonction pour recuperer les details de chaque livre sur l'url de chaque livre
+def book_details(book_url):
+    response = requests.get(book_url).text
+    book_doc = BeautifulSoup(response, "html.parser")
+
+    upc = book_doc.find(text="UPC").next.text
+    return {
+        'upc': upc,
+    }
+
+#faire une boucle pour récupèrer les infos de chaque livre
 all_books_data = []
 for book in all_books:
-    #book_title = book.h3.a['title']
+    book_title = book.h3.a['title']
     book_url = book.h3.a['href'].replace("../../../",MAIN_URL+ '/catalogue' '/')
-    all_books_data.append(book_url)
+    book_detail = book_details(book_url)
+    book_data = {
+        'Title': book_title,
+        'url': book_url,
+        'upc': book_detail['upc']
+    }
+    all_books_data.append(book_data)
 print("books:", all_books_data)
+
 
 # voir le code parsé par beautiful soup
 #print(doc)
