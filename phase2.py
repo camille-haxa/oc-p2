7f76e9c194bba45a16cad3e0df023be3b8320768
camille-haxa <camille.haxa@proton.me>
2026-05-18 11:01:23 +0200

    feat: refacto boucle infos livre, ajout fonction ecriture csv et refacto fonction book details

--- phase2.py
+++ phase2.py
@@ -32,6 +32,8 @@ def book_details(book_url):
     rating = book_doc.find(class_="col-sm-6 product_main").find('p', class_="star-rating")['class'][1]
     image = book_doc.img['src'].replace("../../",MAIN_URL+"/")
     return {
+        'title': book_title,
+        'url': book_url,
         'upc': upc,
         'prix TTC': price_with_tax,
         'prix HT': price_no_tax,
@@ -42,27 +44,30 @@ def book_details(book_url):
         'image url': image
     }
 
+#definir une fonction pour inscrire les données dans un fichier csv
+def ecriture_csv(data, filename):
+
+#créer un nouveau fichier et instancier un objet writer
+    with open('cat_data.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(['product_page_url', 'universal_product_code(upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url' ])
+    #créer une ligne avec les données
+        for book in data:
+            writer.writerow([book['url'], book['upc'], book['title'], book['prix TTC'], book['prix HT'], book['number available'], book['product description'], book['category'], book['rating'], book['image url']])
+
+
 #faire une boucle pour récupèrer les infos de chaque livre
 all_books_data = []
 for book in all_books:
     book_title = book.h3.a['title']
     book_url = book.h3.a['href'].replace("../../../",MAIN_URL+ '/catalogue' '/')
     #appeler la fonction pour recuperer le detail de chaque livre sur l'url correspondante:
-    book_detail = book_details(book_url)
-    book_data = {
-        'Title': book_title,
-        'url': book_url,
-        'upc': book_detail['upc'],
-        'prix TTC': book_detail['prix TTC'],
-        'prix HT': book_detail['prix HT'],
-        'nombre en stock': book_detail['number available'],
-        'description': book_detail['product description'],
-        'catégorie': book_detail['category'],
-        'rating': book_detail['rating'],
-        'url image': book_detail['image url']
-    }
+    book_data = book_details(book_url)
+    
     all_books_data.append(book_data)
-print("books:", all_books_data)
+#print("books:", all_books_data)
+
+ecriture_csv(all_books_data, "cat_data.csv")
 
 
 # voir le code parsé par beautiful soup
